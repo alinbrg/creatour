7b8110da0831348eb6285cc9ec8d7cfc5047d109
alina <alina.bregvadze@gmail.com>
2021-12-16 11:45:29 +0400

    first page

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -1,41 +1,47 @@
 const mainSlider = new Swiper(".main-slider__swiper", {
     // Optional parameters
     direction: "horizontal",
-    loop: true,
-  
+    loop: false,
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction:false
+    // },
     // If we need pagination
     pagination: {
-      el: ".swiper-pagination",
+      el: ".main-slider__pgn",
       clickable: true
     },
 
 });
 
-var swiper = new Swiper(".services-swiper", {
+const swiper = new Swiper(".services-swiper", {
   loop: false,
-  spaceBetween: 10,
+  spaceBetween: 25,
   freeMode: true,
   watchSlidesProgress: true,
   breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 2,
-      spaceBetween: 10
+      spaceBetween: 15,
     },
     // when window width is >= 1280px
     1280: {
       slidesPerView: 6,
-      spaceBetween: 10
     }
   },
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
+    el: ".services-slider__pgn",
+    clickable: true,
+  }
 });
 
-var swiper2 = new Swiper(".services-swiper2", {
-  loop: false,
+const swiper2 = new Swiper(".services-swiper2", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction:false
+  },
   spaceBetween: 10,
   navigation: {
     nextEl: ".swiper-button-next",
@@ -47,3 +53,58 @@ var swiper2 = new Swiper(".services-swiper2", {
   
 });
 
+// add id for each block
+let countriesList = [...document.querySelectorAll('.countries__slide')];
+const countries = new Swiper(".countries__swiper", {
+  spaceBetween: 10,
+
+  pagination: {
+    el: ".countries__pgn",
+    clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets:3,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + countriesList[index].id + "</span>";
+    }
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      direction: "horizontal",
+      allowTouchMove: false,
+      pagination: {
+        dynamicBullets: true,
+        dynamicMainBullets:11,
+      }
+    },
+    // when window width is >= 1280px
+    1280: {
+      direction: "vertical",
+      pagination: {
+        dynamicBullets: true,
+        dynamicMainBullets:3,
+      }
+    }
+  }
+});
+
+
+let scrollDown = document.querySelector('.scroll-down');
+let servicies = document.getElementById('services')
+if(scrollDown){
+  scrollDown.addEventListener('click', (e)=>{
+    e.preventDefault();
+    servicies.scrollIntoView();
+  })
+}
+
+let countriesPgn = document.querySelectorAll('.countries__pgn .swiper-pagination-bullet');
+if(window.innerWidth < 1280){
+  countriesPgn.forEach(element => {
+    if(element.innerText == "France" || element.innerText == "france"){
+      element.style.order = (-1);
+      element.click();
+    }
+  });
+}
